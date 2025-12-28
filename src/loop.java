7c509e67620717c66b5ec67a103e52bf4ce98cf7
Amit Rathod <amitsrathod1011@gmail.com>
2025-12-28 23:28:40 +0530

    Add new practices.java file

---
+++ src/loop.java
@@ -0,0 +1,152 @@
+//Print numbers from 1 to 10 using a loop.
+/*public class loop {
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=10;++i) {
+			System.out.println(i);
+		}
+	}
+}
+*/
+
+
+//Print numbers from 10 to 1.
+/*public class loop{
+	public static void main(String[] args) {
+		
+		for(int i=10; i>=1; i--) {
+			System.out.println(i);
+		}
+	}
+}
+*/
+
+//Print all even numbers between 1 and 50.
+/*public class loop{
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=50; i++) {
+			if (i%2==0)
+			System.out.println(i);
+		}
+	}
+}
+*/
+
+//Print all odd numbers between 1 and 50.
+/*public class loop{
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=50; i++) {
+			if (i%2==1)
+			System.out.println(i);
+		}
+	}
+}
+*/
+
+
+//Print the table of 5 using a loop.
+/*public class loop{
+	public static void main(String[] args) {
+		
+		int i=1;
+		do {
+			System.out.println(5*i);
+			i++;
+		}while(i<=10);
+	}
+}
+*/
+
+
+//Print the table of any number (e.g., n = 7).
+/*public class loop{
+	public static void main(String[] args) {
+		
+		int i=1;
+		while(i<=10){
+			System.out.println(7*i);
+			i++;
+		}
+	}
+}
+*/
+
+//Print Hello 10 times.
+/*public class loop{
+	public static void main(String[] args) {
+		
+		int i=1;
+		while(i<=10){
+			System.out.println("Hello");
+			i++;
+		}
+	}
+}
+*/
+
+
+//Print the sum of first 10 natural numbers.
+/*public class loop{
+	public static void main(String[] args) {
+		int sum =0;
+		for(int i=1; i<=10; i++) {
+			sum=sum+i;	
+		}System.out.println(sum);
+	}
+}
+*/
+
+
+//Print the sum of even numbers from 1 to 100.
+/*public class loop{
+	public static void main(String[] args) {
+		
+		int sum =0;
+		for(int i=1; i<=100; i++) {
+			if(i%2==0) 
+			sum=sum+i;
+			 
+		}System.out.println(sum);
+		
+	}
+}
+*/
+
+
+//Print the sum of odd numbers from 1 to 100.
+/*public class loop{
+	public static void main(String[] args) {
+		
+		int sum =0;
+		for(int i=1; i<=100; i++) {
+			if(i%2==1) 
+			sum=sum+i;
+			 
+		}System.out.println(sum);
+		
+	}
+}
+*/
+
+
+//Count how many numbers between 1 and 100 are divisible by 5.
+/*public class loop{
+	public static void main(String[] args) {
+		int count = 0;
+		
+		for(int i=1; i<=100; i++) {
+			if(i%5==0) 
+				count++;
+			}System.out.println(count);
+		}
+	}
+*/
+
+
+
+
+
+
+
